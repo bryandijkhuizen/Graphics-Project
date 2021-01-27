3de340d93b4811588412bd0c8f66f002d491d08b
ElvedinI <ElvedinIbric@users.noreply.github.com>
2021-01-27 20:42:07 +0100

    javascript opgeruimed uit html

---
+++ AmazonSimulatie/target/classes/static/RobotMaterials.js
@@ -0,0 +1,9 @@
+import * as THREE from "./three.js-master/build/three.module.js";
+const robotMaterials = [new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/robot_side.png"), side: THREE.DoubleSide, }), //LEFT
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/robot_side.png"), side: THREE.DoubleSide, }), //RIGHT
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/robot_top.png"), side: THREE.DoubleSide, }), //TOP
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/robot_bottom.png"), side: THREE.DoubleSide, }), //BOTTOM
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/robot_front.png"), side: THREE.DoubleSide, }), //FRONT
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/robot_front.png"), side: THREE.DoubleSide, }), //BACK
+];
+export default robotMaterials;
