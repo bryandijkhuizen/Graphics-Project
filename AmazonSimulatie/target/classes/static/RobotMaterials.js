0f9ca141f08efc884da1526c4e64fffb375b4f90
ElvedinI <ElvedinIbric@users.noreply.github.com>
2021-01-29 22:52:51 +0100

    cleancleancleanup

--- AmazonSimulatie/target/classes/static/RobotMaterials.js
+++ AmazonSimulatie/target/classes/static/RobotMaterials.js
@@ -1,9 +1,12 @@
 import * as THREE from "./three.js-master/build/three.module.js";
-const robotMaterials = [new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/robot_side.png"), side: THREE.DoubleSide, }), //LEFT
-    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/robot_side.png"), side: THREE.DoubleSide, }), //RIGHT
-    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/robot_top.png"), side: THREE.DoubleSide, }), //TOP
-    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/robot_bottom.png"), side: THREE.DoubleSide, }), //BOTTOM
-    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/robot_front.png"), side: THREE.DoubleSide, }), //FRONT
-    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("textures/robot_front.png"), side: THREE.DoubleSide, }), //BACK
+
+const RobotMaterials = [
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/robot_side.png"),side: THREE.DoubleSide,}), //LEFT
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/robot_side.png"), side: THREE.DoubleSide,}), //RIGHT
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/robot_top.png"), side: THREE.DoubleSide,}), //TOP
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/robot_bottom.png"),side: THREE.DoubleSide,}), //BOTTOM
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/robot_front.png"),side: THREE.DoubleSide,}), //FRONT
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/robot_front.png"),side: THREE.DoubleSide,}), //BACK
 ];
-export default robotMaterials;
+
+export default RobotMaterials
--- AmazonSimulatie/target/classes/static/Script.js
+++ AmazonSimulatie/target/classes/static/Script.js
@@ -1,24 +1,19 @@
 import * as THREE from "./three.js-master/build/three.module.js";
 import { GLTFLoader } from "./three.js-master/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "./three.js-master/examples/jsm/controls/OrbitControls.js";
-import robotCreator from "./RobotCreator.js";
-import robotMaterials from "./RobotMaterials.js";
-import createStellage from "./Stellage.js";
-import createWarehouse from "./Warehouse.js";
-import createTruck from "./Truck.js";
+import RobotCreator from "./RobotCreator.js";
+import RobotMaterials from "./RobotMaterials.js";
 
-let gltfLoader = new GLTFLoader();
-let socket;
+let socket, gltfLoader = new GLTFLoader();
 
 function parseCommand(input = "") {
     console.log(JSON.parse(input));
     return JSON.parse(input);
 }
 
+
 window.onload = function () {
-    var camera, scene, renderer;
-    var cameraControls;
-    var worldObjects = {};
+    let camera, scene, renderer, cameraControls, worldObjects = {};
 
     function init() {
         scene = new THREE.Scene();
@@ -36,15 +31,17 @@ window.onload = function () {
         camera.position.x = 15;
         cameraControls.update();
         window.addEventListener("resize", onWindowResize, false);
-        var hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 4);
+        let hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 4);
         hemiLight.intensity = 5;
         scene.add(hemiLight);
     }
+
     function onWindowResize() {
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
         renderer.setSize(window.innerWidth, window.innerHeight);
     }
+
     function animate() {
         requestAnimationFrame(animate);
         cameraControls.update();
@@ -58,7 +55,7 @@ window.onload = function () {
     socket = new WebSocket("ws://" + window.location.hostname + ":" + window.location.port + "/connectToSimulation");
     socket.onmessage = function (event) {
         //Hier wordt het commando dat vanuit de server wordt gegeven uit elkaar gehaald
-        var command = parseCommand(event.data);
+        let command = parseCommand(event.data);
 
         //Wanneer het commando is "object_update", dan wordt deze code uitgevoerd. Bekijk ook de servercode om dit goed te begrijpen.
         if (command.command == "object_update") {
@@ -67,26 +64,53 @@ window.onload = function () {
             if (Object.keys(worldObjects).indexOf(command.parameters.uuid) < 0) {
                 //Wanneer het object een robot is, wordt de code hieronder uitgevoerd.
                 if (command.parameters.type == "robot") {
-                    let robot = robotCreator(robotMaterials);
-                    var group = new THREE.Group();
+                    let robot = RobotCreator(RobotMaterials), group = new THREE.Group()
                     group.add(robot);
                     scene.add(group);
                     worldObjects[command.parameters.uuid] = group;
                 }
                 if (command.parameters.type == "stellage") {
                     worldObjects[command.parameters.uuid] = null;
-                    createStellage(worldObjects, scene, gltfLoader, command);
+                    gltfLoader.load("./gltf/Stellage.glb", function (gltf) {
+                        let stellage = gltf.scene, group = new THREE.Group();
+                        group.add(stellage);
+                        scene.add(group);
+                        worldObjects[command.parameters.uuid] = group;
+                    });
                 }
                 if (command.parameters.type == "warehouse") {
                     worldObjects[command.parameters.uuid] = null;
-                    createWarehouse(worldObjects, scene, gltfLoader, command);
+                    gltfLoader.load("./gltf/warehouse2.glb", function (gltf) {
+                        let warehouse = gltf.scene, group = new THREE.Group();
+                        warehouse.scale.x = 1.666;
+                        warehouse.scale.y = 1.4;
+                        warehouse.scale.z = 1.2;
+                        warehouse.position.x = 37;
+                        warehouse.position.z = 26.5;
+                        warehouse.rotation.y = 3.15;
+                        group.add(warehouse);
+                        scene.add(group);
+                        worldObjects[command.parameters.uuid] = group;
+                    });
                 }
                 if (command.parameters.type == "truck") {
                     worldObjects[command.parameters.uuid] = null;
-                    createTruck(worldObjects, scene, gltfLoader, command);
+                    gltfLoader.load("./gltf/Truck.glb", function (gltf) {
+                        let truck = gltf.scene, group = new THREE.Group();
+                        truck.scale.x = 100;
+                        truck.scale.y = 100;
+                        truck.scale.z = 100;
+                        truck.position.x = -43.7;
+                        truck.position.y = -1.71;
+                        truck.position.z = 9.22;
+                        truck.rotation.y = 3.16;
+                        group.add(truck);
+                        scene.add(group);
+                        worldObjects[command.parameters.uuid] = group;
+                    });
                 }
             }
-            var object = worldObjects[command.parameters.uuid];
+            let object = worldObjects[command.parameters.uuid];
             if (object != null) {
                 object.position.x = command.parameters.x;
                 object.position.y = command.parameters.y;
